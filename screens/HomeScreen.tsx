482d0539e8fab93b109fda6e3279b3e9683b6476
cindykandie <cindyjk6@gmail.com>
2023-10-27 17:13:55 +0300

    Add the HomeScreen UI

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -6,22 +6,29 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 const HomeScreen: React.FC = ({ navigation }) => {
   return (
     <View>
-      <Text>OverView</Text>
+      <Text alignSelf={'center'} fontSize={23} textAlign={'center'} px='10'>My Money Buddy, Budget On The Go!</Text>
       <Box>
-        <Text>Budget On The Go</Text>
+        <Box py='20px' borderBottomWidth={2}>
         <TouchableOpacity onPress={() => navigation.navigate('Budget')}>
           <Image
             source={require('../assets/budget.png')}
             alt="Budget"
-            size="20" 
+            size="200" 
+            alignSelf={'center'}
           />
+          <Text bg={'blue.400'} mx={'40px'} borderRadius={'10px'} py={'10px'} color={'white'} textAlign={'center'} fontSize={20}>Budget AnyTime. AnyWhere</Text>
+        
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('TrackExpense')}>
+        </Box>
+
+        <TouchableOpacity  onPress={() => navigation.navigate('TrackExpense')} >
           <Image
-            source={require('../assets/budget.png')} 
+            source={require('../assets/track1.png')} 
             alt="Track Expense"
-            size="20" 
+            size="200" 
+            alignSelf={'center'}
           />
+          <Text  bg={'blue.400'} my={'10px'} mx={'40px'} borderRadius={'10px'} py={'10px'} color={'white'} textAlign={'center'} fontSize={20}>Track your Expenses</Text>
         </TouchableOpacity>
       </Box>
     </View>
